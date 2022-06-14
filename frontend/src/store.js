1ba9b6b5346c1ea3b90edd304280c37868e3022a
Gaurav Pandey <pandey@Gauravs-MacBook-Pro.local>
2022-06-14 12:36:44 +0545

    Initial commit

---
+++ frontend/src/store.js
@@ -0,0 +1,47 @@
+//this is the most important file for using the redux as state management tool
+//this file should be created directly in the frontend folder
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { studentListReducer } from './reducers/studentReducers'
+import { userLoginReducer } from './reducers/userReducers'
+import {
+  studentClassListReducer,
+  studentSearchReducer,
+  studentRegisterReducer,
+  studentDeleteReducer,
+  studentAttendanceReducer,
+  studentFeesReducer,
+} from './reducers/studentReducers'
+import {
+  allIncomeReducer,
+  allSalaryReducer,
+} from './reducers/miscellaneousReducers'
+const reducer = combineReducers({
+  studentList: studentListReducer,
+  studentClassList: studentClassListReducer,
+  studentSearch: studentSearchReducer,
+  userLogin: userLoginReducer,
+  studentRegister: studentRegisterReducer,
+  studentDelete: studentDeleteReducer,
+  studentAttendance: studentAttendanceReducer,
+  studentFees: studentFeesReducer,
+  allIncome: allIncomeReducer,
+  allSalary: allSalaryReducer,
+})
+const userInfoFromStorage = localStorage.getItem('userCred')
+  ? JSON.parse(localStorage.getItem('userCred'))
+  : null
+
+//remember the above should be null
+const initialState = {
+  userLogin: { userCred: userInfoFromStorage },
+}
+const middleware = [thunk]
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
